8b429d0ee9339c082a85b18e5688ed891025a959
runningwater <ynwdlxm@163.com>
2016-12-07 16:54:13 +0800

    获取  Json 数据

--- app/src/main/java/com/android/elabcare/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/android/elabcare/photogallery/PhotoGalleryFragment.java
@@ -6,14 +6,16 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.android.elabcare.photogallery.model.GalleryItem;
 import com.android.elabcare.photogallery.net.FlickrFetChr;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by runningwater on 2016/12/6.
@@ -23,6 +25,7 @@ public class PhotoGalleryFragment extends Fragment {
     private static final String TAG = "PhotoGalleryFragment";
 
     private RecyclerView mPhotoRecyclerView;
+    private List<GalleryItem> mItems = new ArrayList<>();
 
     public static Fragment newInstance() {
         return new PhotoGalleryFragment();
@@ -43,21 +46,85 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_photo_gallery_recycler_view);
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
+        setupAdapter();
+
         return v;
     }
 
-    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+    private void setupAdapter() {
+        if (isAdded()) {
+            mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
+        }
+    }
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+
+        private TextView mTitleTextView;
+
+        public PhotoHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+
+        public void bindGalleryItem(GalleryItem item) {
+            mTitleTextView.setText(item.toString());
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+
+        private List<GalleryItem> mGalleryItems;
+
+        public PhotoAdapter(List<GalleryItem> galleryItems) {
+            mGalleryItems = galleryItems;
+        }
 
         @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                String result = new FlickrFetChr().getUrlString("https://www.bignerdranch.com");
-                Log.i(TAG, "Fetch contents of URL: " + result);
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.e(TAG, "Failed to fetch URL: ", e);
-            }
-            return  null;
+        public PhotoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            TextView textView = new TextView(getActivity());
+            return new PhotoHolder(textView);
+        }
+
+        @Override
+        public void onBindViewHolder(PhotoHolder holder, int position) {
+            GalleryItem galleryItem = mGalleryItems.get(position);
+            holder.bindGalleryItem(galleryItem);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mGalleryItems.size();
+        }
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, List<GalleryItem>> {
+        /**
+         * <p>Runs on the UI thread after {@link #doInBackground}. The
+         * specified result is the value returned by {@link #doInBackground}.</p>
+         * <p>
+         * <p>This method won't be invoked if the task was cancelled.</p>
+         *
+         * @param galleryItems The result of the operation computed by {@link #doInBackground}.
+         * @see #onPreExecute
+         * @see #doInBackground
+         * @see #onCancelled(Object)
+         */
+        @Override
+        protected void onPostExecute(List<GalleryItem> galleryItems) {
+            mItems = galleryItems;
+            setupAdapter();
+        }
+
+        @Override
+        protected List<GalleryItem> doInBackground(Void... params) {
+//            try {
+//                String result = new FlickrFetChr().getUrlString("https://www.bignerdranch.com");
+//                Log.i(TAG, "Fetch contents of URL: " + result);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                Log.e(TAG, "Failed to fetch URL: ", e);
+//            }
+            return new FlickrFetChr().fetchItems();
         }
     }
 }
--- app/src/main/java/com/android/elabcare/photogallery/net/FlickrFetChr.java
+++ app/src/main/java/com/android/elabcare/photogallery/net/FlickrFetChr.java
@@ -1,16 +1,30 @@
 package com.android.elabcare.photogallery.net;
 
+import android.net.Uri;
+import android.util.Log;
+
+import com.android.elabcare.photogallery.model.GalleryItem;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by runningwater on 2016/12/6.
  */
 
 public class FlickrFetChr {
+    private static final String TAG = "FlickrFetChr";
+    private static final String API_KEY = "90b626dadf8e36eee3b7499d8411844e";
+
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -36,5 +50,86 @@ public class FlickrFetChr {
 
     public String getUrlString(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    public List<GalleryItem> fetchItems() {
+        List<GalleryItem> items = new ArrayList<GalleryItem>();
+        String url = Uri.parse("https://api.flickr.com/services/rest/")
+                .buildUpon()
+                .appendQueryParameter("method", "flickr.photos.getRecent")
+                .appendQueryParameter("api_key", API_KEY)
+                .appendQueryParameter("extras", "url_s")
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
+                .build().toString();
+        try {
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+            JSONObject jsonBody = new JSONObject(jsonString);
+            parseItems(items, jsonBody);
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+            Log.e(TAG, "Failed to fetch items", e);
+        } catch (JSONException je) {
+            Log.e(TAG, "Failed to parse JSON", je);
+        }
+        return items;
+    }
+
+    /**
+     * 解析 Json 数据
+     *
+     * @param items
+     * @param jsonBody 数据，期格式如下
+     *                 {
+     *                 "photos": {
+     *                 "page": 1,
+     *                 "pages": 10,
+     *                 "perpage": 100,
+     *                 "total": 1000,
+     *                 "photo": [{
+     *                 "id": "30633471944",
+     *                 "owner": "21022123@N04",
+     *                 "secret": "586b2d9c32",
+     *                 "server": "5544",
+     *                 "farm": 6,
+     *                 "title": "Baltimore Museum of Art",
+     *                 "ispublic": 1,
+     *                 "isfriend": 0,
+     *                 "isfamily": 0
+     *                 },
+     *                 {
+     *                 "id": "31474846625",
+     *                 "owner": "145532591@N02",
+     *                 "secret": "eda462a3c7",
+     *                 "server": "5524",
+     *                 "farm": 6,
+     *                 "title": "20161107_095207",
+     *                 "ispublic": 1,
+     *                 "isfriend": 0,
+     *                 "isfamily": 0
+     *                 }]
+     *                 },
+     *                 "stat": "ok"
+     *                 }
+     */
+    private void parseItems(List<GalleryItem> items, JSONObject jsonBody) throws JSONException {
+        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+        JSONArray photoJsonArray = photosJsonObject.getJSONArray("photo");
+
+        for (int i = 0; i < photoJsonArray.length(); i++) {
+            JSONObject photoJsonObject = photoJsonArray.getJSONObject(i);
+
+            GalleryItem item = new GalleryItem();
+            item.setId(photoJsonObject.getString("id"));
+            item.setCaption(photoJsonObject.getString("title"));
+
+            if (!photoJsonObject.has("url_s")) {
+                continue;
+            }
+            item.setUrl(photoJsonObject.getString("url_s"));
+            items.add(item);
+        }
     }
 }
